ea253a759fd7ef9ed525b670c1a5867eef2ef00d
NazmulHamim12 <149864562+NazmulHamim12@users.noreply.github.com>
2025-10-27 00:42:03 +0600

    Refactor taskinfo URL to use class-based view
    Updated taskinfo URL to use class-based view and added detail for primary key.

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -5,6 +5,7 @@ from api import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('taskinfo/',views.taskinfo),
-    path('taskadd/',views.task_add)
+    path('taskinfo/',views.Taskinfo.as_view()),
+    path('taskinfo/<int:pk>',views.Taskinfo.as_view()),
+    #path('taskadd/',views.task_add)
 ]
