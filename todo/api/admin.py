7a36fe221bbb76f1e095396e73242326fe1dd1ce
NazmulHamim12 <149864562+NazmulHamim12@users.noreply.github.com>
2025-10-27 00:40:03 +0600

    Update admin.py

--- todo/api/admin.py
+++ todo/api/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from . models import Todo
 # Register your models here.
-admin.site.register(Todo)
+#admin.site.register(Todo)
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display=['id','task','prio']
