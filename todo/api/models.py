153ec8548eaff66f956f8fb2cdf89bf1cd53d4c4
NazmulHamim12 <149864562+NazmulHamim12@users.noreply.github.com>
2025-10-27 00:41:08 +0600

    Add priority field to Todo model
    Added 'prio' field to Todo model for task priority.

--- todo/api/models.py
+++ todo/api/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Todo(models.Model):
     task=models.CharField(max_length=100)
-    
+    prio=models.CharField(max_length=30,blank=True)
     def __str__(self):
         return self.task
