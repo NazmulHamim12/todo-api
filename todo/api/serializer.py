d357e2f345c3cd7300fe4f8a546d2f93420d90fe
NazmulHamim12 <149864562+NazmulHamim12@users.noreply.github.com>
2025-10-28 16:41:11 +0600

    Update serializer.py

--- todo/api/serializer.py
+++ todo/api/serializer.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
-from . models import Todo
+from . models import Todo,Sing_up
 
 
 
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model=Todo
-        fields=['id','task','prio']
+        fields=['id','user','task','prio']
+        
+        
+        
+class SingupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Sing_up
+        fields=['id','name','email','password']
+        
+        
+        
